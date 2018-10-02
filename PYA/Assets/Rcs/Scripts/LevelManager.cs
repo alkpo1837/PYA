b029780d5b95c0b952d0077c4689a599fe7fdf3d
alkpo1837 <capo.arthur@gmail.com>
2018-10-02 09:12:11 +0200

    fix gitignore, there is a little scene

---
+++ PYA/Assets/Rcs/Scripts/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("References")]
+    public Player PlayerI;
+
+    [Header("Elements")]
+    public UIWidget PlayButton;
+
+    private void Start()
+    {
+        UIEventListener.Get(PlayButton.gameObject).onClick = ClickOnPlayButton;
+    }
+
+    private void ClickOnPlayButton(GameObject go)
+    {
+        PlayerI.DebugGoRight();
+    }
+}
---
+++ PYA/Assets/Rcs/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private bool _isMoving = false;
+
+    private void Update()
+    {
+        if (_isMoving == true)
+        {
+            transform.localPosition += Vector3.right * 250.0f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localPosition += Vector3.right * 10.0f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localPosition += Vector3.left * 10.0f * Time.deltaTime;
+        }
+    }
+
+    public void DebugGoRight()
+    {
+        _isMoving = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Objective")
+        {
+            FinishLevel();
+        }
+    }
+
+    private void FinishLevel()
+    {
+        Debug.Log(gameObject + " : objective FOUND !");
+
+        _isMoving = false;
+    }
+}
